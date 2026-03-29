f8a4d4a4c11e4a4314707c3fbed3725ac771e789
yash tanwar <yashtanwar810@gmail.com>
2026-03-29 20:18:28 +0530

    dependencies intallation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
   
   return (
     <>
-      <h1 className="text-5xl font-bold">hello amit bhai</h1>
+      <h1 className="text-5xl font-bold text-amber-400">hello amit bhai</h1>
     </>
   )
 }
